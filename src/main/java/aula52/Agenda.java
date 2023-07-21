832716ebddb41f55f016ef3c57faa4f9d2917a7a
hanna tuburnine <hannatbr@gmail.com>
2023-07-21 11:45:56 -0300

    finalizacao do exercicio loiane

--- src/main/java/aula52/Agenda.java
+++ src/main/java/aula52/Agenda.java
@@ -25,6 +25,18 @@ public class Agenda extends Contato {
         }
     }
 
+    public int consultaContato(String nome) throws ContatoNaoExisteException {
+
+        for (int i = 0; i<contatos.length; i++){
+            if (contatos[i] != null){
+                if (contatos[i].getNome().equalsIgnoreCase(nome)){
+                    return i;
+                }
+            }
+        }
+        throw new ContatoNaoExisteException(nome);
+    }
+
 
     @Override
     public String toString(){
--- src/main/java/aula52/Teste.java
+++ src/main/java/aula52/Teste.java
@@ -1,13 +1,45 @@
 package aula52;
 
+import java.util.Scanner;
+
 public class Teste {
 
     public static void main(String[] args) {
 
-        Contato c1 = new Contato();
-        System.out.println(c1.getId());
+        Scanner scan = new Scanner(System.in);
 
-        Contato c2 = new Contato();
-        System.out.println(c2.getId());
+        int opcao = obterOpcaoMenu(scan);
+
     }
-}
+    public static int obterOpcaoMenu(Scanner scan) {
+
+        boolean entradaValida = false;
+        int opcao = 3;
+
+        while (!entradaValida){
+            System.out.println("Digite a opção desejada: ");
+            System.out.println("1: Consultar contado");
+            System.out.println("2: Adicionar contato");
+            System.out.println("3: Sair");
+            opcao = scan.nextInt();
+
+            try {
+                opcao = scan.nextInt();
+
+                if (opcao == 1 || opcao == 2 || opcao == 3){
+                    entradaValida = true;
+                } else {
+                    throw new Exception("Entrada inválida");
+                }
+            }
+            catch (Exception e){
+                System.out.println("Entrada inválida");
+            }
+        }
+
+
+
+            return opcao;
+        }
+    }
+
